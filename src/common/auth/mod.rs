c3c89e7e405934307c8fb21bb343708e9d828188
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2026-09-23 06:08:29 +0300

    dry

--- src/common/auth/mod.rs
+++ src/common/auth/mod.rs
@@ -205,12 +205,6 @@ impl AuthKeys {
                 ));
             }
 
-            if blacklist_matches {
-                return Err(AuthError::Forbidden(
-                    "This path is blacklisted by config".to_string(),
-                ));
-            }
-
             return Ok((access, InferenceToken(sub), AuthType::Jwt, subject));
         }
 
